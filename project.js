5197a493f121cc1c9dd814bbad11e50e20fac294
Mikkal1 <mramcharitar2@gmail.com>
2024-03-25 21:10:58 -0400

    Initial commit

---
+++ project.js
@@ -0,0 +1,17 @@
+// 1. Deposit Some Money.
+// 2. Determine number of lines to bet on.
+// 3. Collect a bet amount.
+// 4. Spin the slot machine.
+// 5. Check if the user won.
+// 6. Give user their winnings.
+// 7. Play again.
+
+
+
+const readlineSync = require("readline-sync");
+
+const deposit = () => {
+   const depositAmount = readlineSync.question("Enter Deposit Amounnt: ");
+};
+
+deposit();
